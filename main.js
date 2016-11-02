e117ae02ef2ff2bfe3cd7d0fefc397986014f205
Paul <pacalabre@gmail.com>
2016-11-02 09:47:18 -0700

    added basic files and created basic layout

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var ctrl = new ScrollMagic.Controller({
+    globalSceneOptions: {
+        triggerHook: 'onLeave'
+    }
+});
+
+
+
+
+$("section").each(function() {
+
+    var name = $(this).attr('id');
+
+    new ScrollMagic.Scene({
+        triggerElement: this
+    })
+    .setPin(this)
+    .addTo(ctrl);
+
+ });
+
+
+
+
