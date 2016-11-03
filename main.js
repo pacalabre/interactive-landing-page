96b6ef2393c4e562ca75c6433f82f9dfbd5c5ecd
Paul <pacalabre@gmail.com>
2016-11-03 10:05:43 -0700

    added footer img

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
-var ctrl = new ScrollMagic.Controller({
-    globalSceneOptions: {
-        triggerHook: 'onLeave'
-    }
-});
+// var ctrl = new ScrollMagic.Controller({
+//     globalSceneOptions: {
+//         triggerHook: 'onLeave'
+//     }
+// });
 
 
 
 
-$("section").each(function() {
-    var name = $(this).attr('id');
-    new ScrollMagic.Scene({
-        triggerElement: this
-    })
-    .setPin(this)
-    .addTo(ctrl);
+// $("section").each(function() {
+//     var name = $(this).attr('id');
+//     new ScrollMagic.Scene({
+//         triggerElement: this
+//     })
+//     .setPin(this)
+//     .addTo(ctrl);
 
- });
+//  });
 
 
 
