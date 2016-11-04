12a70685c3a089351d4a02b2f8f1d1e8928366a7
Paul <pacalabre@gmail.com>
2016-11-04 11:07:07 -0700

    changed duration of animation

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var ctrl = new ScrollMagic.Controller({
     globalSceneOptions: {
         triggerHook: 'onLeave'
@@ -18,11 +17,43 @@ $("section").each(function() {
  });
 
 var footerScene = new ScrollMagic.Scene({
-  triggerElement: "#footerTrigger"
+  triggerElement: "#footerTrigger",
 })
 
-.setTween("#footerTrigger", 0.5, { backgroundColor: '#003b40'})
+.setTween("#element", 0.75, { backgroundColor: '#003b40'})
 .addIndicators({name: 'animate footer'})
 .addTo(ctrl);
+
+
+
+
+// var ctrl = new ScrollMagic.Controller({
+//     globalSceneOptions: {
+//         triggerHook: 'onLeave'
+//     }
+// });
+
+
+// //WORKING Scroll Magic Code
+// $("section").each(function() {
+//     var name = $(this).attr('id');
+//     new ScrollMagic.Scene({
+//         triggerElement: this
+//     })
+//     .setPin(this)
+//     .addTo(ctrl);
+
+//  });
+
+// var footerScene = new ScrollMagic.Scene({
+//   triggerElement: "#footerTrigger",
+//   triggerHook: 0,
+//   duration: 0.5
+//   // triggerHook: 0
+// })
+
+// .setTween("#footerTrigger", 0.5, { backgroundColor: '#003b40'})
+// .addIndicators({name: 'animate it'})
+// .addTo(ctrl);
 
 
